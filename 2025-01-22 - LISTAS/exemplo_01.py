b5aad8c346d059c4fdc2ba03de5c2543ecb27c3a
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2025-01-22 18:35:20 -0300

    LISTAS - Exemplos: FILTER(), SORT() e Funções Lambda

---
+++ 2025-01-22 - LISTAS/exemplo_01.py
@@ -0,0 +1,20 @@
+'''
+   EXEMPLO 01:
+
+   Criar um programa que importe a variavel clubes contida no arquivo brasileirao_serie_a.py
+
+   Uma vez importada a variavel, criar uma lista onde cada elemento contem uma sub-lista. 
+   Os dados de cada sub-lista estão organizados da seguinte forma:
+   
+   [clube, ano, vitorias, empates, derrotas, gols_pro, gols_contra]
+
+   Em seguida o programa deve solicitar o ano da competição e exibir a classificação final 
+   do campeonato relativo ao ano informado. 
+
+   Ao exibir a classificação, os dados devem ser exibidos da seguinte forma:
+
+   01. [clube] [pontos] [vitorias] [empates] [derrotas] [gols_pro] [gols_contra] [saldo_gols] [aproveitamento]
+   02. [clube] [pontos] [vitorias] [empates] [derrotas] [gols_pro] [gols_contra] [saldo_gols] [aproveitamento]
+   ...
+   20. [clube] [pontos] [vitorias] [empates] [derrotas] [gols_pro] [gols_contra] [saldo_gols] [aproveitamento]
+'''
