262d952ff3be306e012155c76c7bd4e9c01c30be
Charles Cesar Magno de Freitas <ccmfreitas@gmail.com>
2025-02-11 19:54:37 -0300

    Exemplos de Dicionários

---
+++ 2025-02-11 - DICIONARIOS/exemplo_05.py
@@ -0,0 +1,11 @@
+'''
+   EXEMPLO 06:
+
+   Usando o dicionário dictAlunos faça um programa que solicite ao usuário a matrícula de um 
+   aluno e as duas notas da disciplina e adicione os valores digitados no dicionário.
+
+   O programa deve continuar solicitando a matrícula e as notas até que o usuário digite a
+   matrícula 0.
+'''
+
+dictAlunos = dict()
